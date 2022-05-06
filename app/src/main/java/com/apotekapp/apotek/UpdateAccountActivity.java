6cce9b6b013ae7270aefcd876dd6f5629cb3f489
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-05-06 22:05:17 +0800

    Migrate to android X :fox_face:

---
+++ app/src/main/java/com/apotekapp/apotek/UpdateAccountActivity.java
@@ -0,0 +1,13 @@
+package com.apotekapp.apotek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_account);
+    }
+}
