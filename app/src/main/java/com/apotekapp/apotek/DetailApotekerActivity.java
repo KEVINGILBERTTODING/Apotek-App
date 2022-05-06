e70c070c540f9b85b5cb491c5db6651c59557c5d
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-05-06 15:00:08 +0800

    detail obat dan detail apoteker complete :pill:

--- app/src/main/java/com/apotekapp/apotek/DetailApotekerActivity.java
+++ app/src/main/java/com/apotekapp/apotek/DetailApotekerActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -32,6 +33,18 @@ public class DetailApotekerActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences(LoginActivity.my_shared_preferences, Context.MODE_PRIVATE);
         username = sharedPreferences.getString("username", null);
+
+
+
+        // Fungsi untuk menyembunyikan navbar
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        // Fungsi untuk menyembunyikan status bar
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
 
