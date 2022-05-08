70e338350d8df55439aad67db7d7b96029a31cf1
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-05-08 11:30:21 +0800

    Apotek app version 1.2 :icecream: :shaved_ice:

--- app/src/main/java/com/apotekapp/apotek/utill/ServerAPI.java
+++ app/src/main/java/com/apotekapp/apotek/utill/ServerAPI.java
@@ -2,28 +2,29 @@ package com.apotekapp.apotek.Utill;
 
 public class ServerAPI {
 
+    // Created by xxivcvnbxx
 
     //Server API untuk data obat
-    public static final String URL_Create_Obat = "http://xxx.xx.xx.xx/apotek/obat/create.php";
-    public static final String URL_Read_Obat = "http://xxx.xx.xx.xx/apotek/obat/read.php";
-    public static final String URL_DELETE_Obat = "http://xxx.xx.xx.xx/apotek/obat/delete.php";
-    public static final String URL_Update_Obat = "http://xxx.xx.xx.xx/apotek/obat/update.php";
+    public static final String URL_Create_Obat = "http://xxx.xx.xx.xx/apotek/obat/create.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Read_Obat = "http://xxx.xx.xx.xx/apotek/obat/read.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_DELETE_Obat = "http://xxx.xx.xx.xx/apotek/obat/delete.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Update_Obat = "http://xxx.xx.xx.xx/apotek/obat/update.php"; // xxx.xx.xx.xx is ur ip adress
 
-    public static final String URL_Read_Obat_Sirup = "http://xxx.xx.xx.xx/apotek/obat/read_sirup.php";
-    public static final String URL_Read_Obat_Tablet = "http://xxx.xx.xx.xx/apotek/obat/read_tablet.php";
-    public static final String URL_Read_Obat_Oles = "http://xxx.xx.xx.xx/apotek/obat/read_obat_oles.php";
-    public static final String URL_Read_Obat_Lain = "http://xxx.xx.xx.xx/apotek/obat/read_obat_lain.php";
+    public static final String URL_Read_Obat_Sirup = "http://xxx.xx.xx.xx/apotek/obat/read_sirup.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Read_Obat_Tablet = "http://xxx.xx.xx.xx/apotek/obat/read_tablet.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Read_Obat_Oles = "http://xxx.xx.xx.xx/apotek/obat/read_obat_oles.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Read_Obat_Lain = "http://xxx.xx.xx.xx/apotek/obat/read_obat_lain.php"; // xxx.xx.xx.xx is ur ip adress
 
 
     // Server API untuk data apoteker
-    public static final String URL_Read_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/read.php";
-    public static final String URL_Create_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/create.php";
-    public static final String URL_DELETE_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/delete.php";
-    public static final String URL_Update_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/update.php";
+    public static final String URL_Read_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/read.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Create_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/create.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_DELETE_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/delete.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Update_Apoteker = "http://xxx.xx.xx.xx/apotek/apoteker/update.php"; // xxx.xx.xx.xx is ur ip adress
 
     // Server API untuk data users
-    public static final String URL_Login = "http://xxx.xx.xx.xx/apotek/users/login.php";
-    public static final String URL_Register = "http://xxx.xx.xx.xx/apotek/users/register.php";
-    public static final String URL_Update_Login = "http://xxx.xx.xx.xx/apotek/users/update_login_user.php";
+    public static final String URL_Login = "http://xxx.xx.xx.xx/apotek/users/login.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Register = "http://xxx.xx.xx.xx/apotek/users/register.php"; // xxx.xx.xx.xx is ur ip adress
+    public static final String URL_Update_Login = "http://xxx.xx.xx.xx/apotek/users/update_login_user.php"; // xxx.xx.xx.xx is ur ip adress
 
 }
